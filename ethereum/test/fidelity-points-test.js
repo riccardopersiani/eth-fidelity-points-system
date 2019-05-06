53e95541385c7dac1ba66cc4ee8ec3b62763efc4
Riccardo Persiani <r.persiani92@gmail.com>
2019-05-07 00:04:27 +0100

    feat(test): prepare new tests

--- ethereum/test/fidelity-points-test.js
+++ ethereum/test/fidelity-points-test.js
@@ -1,13 +1,10 @@
 const {
-  waitForEvent,
-  getExternalVariable
+    waitForEvent,
 } = require('./utils')
 
-const Web3 = require('web3')
-const randomExample = artifacts.require('./FidelityPoints.sol')
-//const RINKEBY_WSS = `wss://rinkeby.infura.io/ws/v3/${getExternalVariable('infuraKey')}`
-const KOVAN_WSS = `wss://kovan.infura.io/ws/v3/${getExternalVariable('infuraKey')}`
-const web3Socket = new Web3(new Web3.providers.WebsocketProvider(KOVAN_WSS))
+const web3 = require('../web3');
+const fidelityPoints = artifacts.require('./FidelityPoints.sol')
+const assert = require('assert');
 
 contract('Fidelity Points Tests', async accounts => {
 
@@ -15,13 +12,108 @@ contract('Fidelity Points Tests', async accounts => {
   const address = accounts[0]
 
   before(async () => (
-    { contract } = await randomExample.deployed(),
+    { contract } = await fidelityPoints.deployed(),
     { methods } = contract,
-    { events } = new web3Socket.eth.Contract(
+    { events } = new web3.eth.Contract(
       contract._jsonInterface,
       contract._address
     )
   ))
+
+  describe('FidelityPoints', () => {
+	  // Check if the contract has been deployed correctly.
+	  it('deploys a FidelityPoints contract', () => {
+		  assert.ok(fidelityPoints.options.address);
+	  });
+
+	  it('marks deployer as the contract owner', async () => {
+		  const owner = await fidelityPoints.methods.owner().call();
+		  assert.equal(accounts[0], owner);
+	  });
+
+	  it('reads token information', async () => {
+		  const SYMBOL = await fidelityPoints.methods.SYMBOL().call();
+		  const NAME = await fidelityPoints.methods.NAME().call();
+		  const DECIMALS = await fidelityPoints.methods.DECIMALS().call();
+		  const RATE = await fidelityPoints.methods.RATE().call();
+		  assert.equal(SYMBOL, 'FIDO');
+		  assert.equal(NAME, 'Fido Coin');
+		  assert.equal(DECIMALS, 18);
+		  assert.equal(RATE, 1000000000000000000);
+	  });
+
+	  it('create tokens', async () => {
+		  let balanceFinal;
+		  let balanceStart = await fidelityPoints.methods.balanceOf(accounts[0]).call();
+		  balanceStart = parseFloat(balanceStart);
+
+		  try {
+			  await fidelityPoints.methods.createTokens().send({
+				  from: accounts[0],
+				  value: '1'
+			  });
+			  balanceFinal = await fidelityPoints.methods.balanceOf(accounts[0]).call();
+			  balanceFinal = parseFloat(balanceFinal);
+
+			  assert(balanceFinal > balanceStart);
+		  } catch (err) {
+			  assert.fail(err);
+		  }
+	  });
+
+	  /*
+	  * start total supply:            1000000000.
+	  * final total supply:   2000000001000000000.
+	  * */
+	  it('totalSupply increment with token creation', async () => {
+		  let totalSupplyStart;
+		  let totalSupplyFinal;
+		  try {
+			  totalSupplyStart = await fidelityPoints.methods.totalSupply().call();
+			  await fidelityPoints.methods.createTokens().send({
+				  value: '2',
+				  from: accounts[0]
+			  });
+			  totalSupplyFinal = await fidelityPoints.methods.totalSupply().call();
+			  assert(totalSupplyFinal>totalSupplyStart);
+
+		  } catch (err) {
+			  assert.fail(err);
+		  }
+	  });
+
+	  it('only manager can create new tokens', async () => {
+		  let balance = await fidelityPoints.methods.balanceOf(accounts[1]).call();
+		  balance = parseFloat(balance);
+		  console.log("starting balance: " + balance);
+
+		  try {
+			  await fidelityPoints.methods.createTokens().send({
+				  value: '20',
+				  from: accounts[1]
+			  });
+		  } catch (err) {
+			  balance = await fidelityPoints.methods.balanceOf(accounts[1]).call();
+			  balance = parseFloat(balance);
+			  console.log("after catch() balance: " + balance);
+			  assert(balance == 0);
+		  }
+	  });
+
+	  it('getSummary', async () => {
+		  try {
+			  const summary = await fidelityPoints.methods.getSummary().call();
+			  console.log("summary[0]: " + summary[0]);
+			  console.log("summary[1]: " + summary[1]);
+			  console.log("summary[2]: " + summary[2]);
+			  console.log("summary[3]: " + summary[3]);
+			  console.log("summary[4]: " + summary[4]);
+			  assert(summary[4],"FIDO");
+		  } catch (err) {
+			  assert.fail(err);
+		  }
+	  });
+  });
 
   it('Should have logged event...', async () => {
     const {
